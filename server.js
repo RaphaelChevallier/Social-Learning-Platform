9722259fa1f3604f3053888e5b3fe3c370031eaf
Raphael Chevallier <raphaelchevallier@hotmail.com>
2019-09-27 01:51:01 -0700

    db

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const env = process.env.NODE_ENV;
 const express = require('express');
 const path = require('path');
 
@@ -11,7 +12,17 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
+const Pool = require('pg').Pool
+if(env == "heroku"){
+  var db = new pg.Client(DATABASE_URL);
+  db.connect();
+  console.log("Heroku db connected") 
+  db.end() //just closing connection
+} else{
+  console.log("connect to local")
+}
+
+
+
 const port = process.env.PORT || 5000;
 app.listen(port);
-
-console.log(`Heroku is working`);
