9e76efb542909f470636ecb69b3bb5e2df4abbcb
Raphael Chevallier <raphaelchevallier@hotmail.com>
2019-09-27 00:06:31 -0700

    version changes

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "yourusername",
-  password: "yourpassword"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
