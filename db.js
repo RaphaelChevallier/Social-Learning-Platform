99c7370679257fafe3f986e4e19f36140a87482b
Raphael Chevallier <raphaelchevallier@hotmail.com>
2019-10-22 01:05:19 -0700

    changes

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const { Client } = require('pg');
 
 function connectDatabase() {
     if (!db) {
-        if(env){
+        if(process.env.USE_HEROKU_DB){
             db = new Client({
               connectionString: process.env.DATABASE_URL,
               ssl: true,
