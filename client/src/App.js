801179bbd266d039bfbc5b87f3ed57e45605b511
tyreln <tyrelnarciso@hotmail.com>
2019-11-19 13:20:40 -0800

    updating

--- client/src/App.js
+++ client/src/App.js
@@ -10,116 +10,33 @@ import "./App.css";
 import Navbar from "./pages/Navbar";
 import SignUpForm from "./pages/signUp/SignUpForm";
 import SignInForm from "./pages/SignInForm";
-import LearnerProfile from "./pages/profilePage/LearnerProfile"
-import { makeStyles } from '@material-ui/core/styles';
+import LearnerProfile from "./pages/profilePage/LearnerProfile";
+import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(2)
   },
   title: {
-    flexGrow: 1,
-  },
+    flexGrow: 1
+  }
 }));
 
 export default function App() {
   return (
     <Router>
-    <div className={classes.root}>
-      <FormGroup>
-        <FormControlLabel
-          control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" />}
-          label={auth ? 'Logout' : 'Login'}
-        />
-      </FormGroup>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Giddy-Up
-          </Typography>
-          <Button color="inherit" onClick = {handleLogout}> {text} </Button>
-            
-          {auth && (
-            <div>
-              <IconButton
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-             
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={open}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-              </Menu>
-            </div>
-          )}
-        </Toolbar>
-      </AppBar>
-    </div>
-    <div className="PageSwitcher">
-          <NavLink
-            to="/sign-in"
-            id="first"
-           
-            activeClassName="PageSwitcher__Item--Active"
-            className="PageSwitcher__Item"
-          >
-            Sign In
-          </NavLink>
-
-          <NavLink
-            exact
-            to="/"
-            activeClassName="PageSwitcher__Item--Active"
-            className="PageSwitcher__Item"
-          >
-            Sign Up
-          </NavLink>
-        </div>
-
-        <Route exact path="/" component={SignUpForm}></Route>
-        <Route path="/sign-in" component={SignInForm}></Route>
-     
-    
-  
-
-
-      <Navbar/>
+      <Navbar />
       <Route path="/sign-in" component={SignInForm}></Route>
       <Route exact path="/" component={SignUpForm}></Route>
-      <Route path ="/profile-page" component={LearnerProfile}></Route>
+      <Route path="/profile-page" component={LearnerProfile}></Route>
     </Router>
   );
 }
-
-   
