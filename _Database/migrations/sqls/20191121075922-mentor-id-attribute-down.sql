8718031a59658eece68e483940afe7a0b8312392
Emmanuel <manuel.gula@yahoo.com>
2019-11-21 00:59:31 -0800

    modifying mentor_id on user and mentor tables

---
+++ _Database/migrations/sqls/20191121075922-mentor-id-attribute-down.sql
@@ -0,0 +1,4 @@
+/* Replace with your SQL commands */
+
+ALTER TABLE "USER" drop mentor_id;
+ALTER TABLE "MENTOR" drop mentor_id;
---
+++ _Database/migrations/sqls/20191121075922-mentor-id-attribute-up.sql
@@ -0,0 +1,4 @@
+/* Replace with your SQL commands */
+
+ALTER TABLE "USER" ADD mentor_id INTEGER ;
+ALTER TABLE "MENTOR" ADD mentor_id INTEGER NOT NULL;
