6c77b51bdeb59d3af888ef6fa6cfac37bb2c90c2
fiyin Oladele <fiyin_oladele@yahoo.com>
2019-11-21 23:45:57 -0800

    confirm => SignIn
    confirm skips Success, goes directly to SignIn

--- client/src/pages/signUp/Success.js
+++ client/src/pages/signUp/Success.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
+import {withRouter} from 'react-router-dom';
+import Button from "@material-ui/core/Button";
 
 
 export class Success extends Component {
   constructor() {
     super();
   }
+  toProfile(){
+    const {history} = this.props;
+    history.push('/profile-page');
+  }
   render(){
+    const onHistory = this.props.history.push('/profile-page');
     return (
       <div>
         <h1>Thank You For Your Submission</h1>
+
+        <br/>
+
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={onHistory}
+        >To the Profile page
+        </Button>
       </div>
     );
   };
@@ -19,4 +35,4 @@ const container = {
     border: '5px orange'
 };
 
-export default Success;
+export default withRouter(Success);
