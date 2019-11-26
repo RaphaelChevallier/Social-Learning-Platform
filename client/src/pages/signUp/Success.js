6b745e20c1cc078ba4b587939f2a3e179ccfc3bc
Raphael Chevallier <raphaelchevallier@hotmail.com>
2019-11-26 14:37:23 -0800

    centering image

--- client/src/pages/signUp/Success.js
+++ client/src/pages/signUp/Success.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 import {withRouter} from 'react-router-dom';
 import Button from "@material-ui/core/Button";
 import Box from '@material-ui/core/Box';
-import { makeStyles } from '@material-ui/core/styles';
-import Avatar from '@material-ui/core/Avatar';
 import Thanks from '../../thankYou.jpg';
+
 
 export class Success extends Component {
   constructor() {
@@ -18,31 +17,28 @@ export class Success extends Component {
 
   render(){
     return (
-      <div style={{ width: '100%' }}>
+      <div style={{ width: '100%', textAlign:'center' }}>
+          <Box display="flex" flexDirection="column"  alignItems="center" p={1} m={1} bgcolor="background.paper">
+            <Box p={1}>
+              <img src={Thanks} alt="ThankYou" style={{ width: '60%', height: '40%' }}/>
+            </Box>
+            <br/>
 
-        <Box display="flex" flexDirection="column" alignItems="center" p={1} m={1} bgcolor="background.paper">
-          <Box p={1}>
-            <img src={Thanks} alt="ThankYou" style={{ alignItems: "center", width: '60%', height: '40%' }}/>
+            <Box p={1}>
+              <h1>Thank You For Your Submission</h1>
+            </Box>
+
+            <br/>
+
+            <Box p={1}>
+              <Button
+                color="primary"
+                variant="contained"
+                onClick={this.toProfile}
+              >To the Profile page
+              </Button>
+            </Box>
           </Box>
-
-          <br/>
-
-          <Box p={1}>
-            <h1>Thank You For Your Submission</h1>
-          </Box>
-
-          <br/>
-
-          <Box p={1}>
-            <Button
-              color="primary"
-              variant="contained"
-              onClick={this.toProfile}
-            >To the Profile page
-            </Button>
-          </Box>
-        </Box>
-
       </div>
     );
   };
