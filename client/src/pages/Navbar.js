50385fd40c01a50320d761bc4f29e5750475ce8f
fiyin Oladele <fiyin_oladele@yahoo.com>
2020-03-10 20:13:05 -0700

    changed the navbar

--- client/src/pages/Navbar.js
+++ client/src/pages/Navbar.js
@@ -102,49 +102,62 @@ const LoginRegSwitch = (
     
   const LogoutProfile = (
     <div>
+
+      <Button color="inherit" onClick={this.toSearch}>Search Users</Button>
+      {
+        (this.state.isMentor!=null)&& (localStorage.usertoken!=null)?
+          <Button color="inherit" onClick={this.toContentCreation.bind(this)}>Content Creation</Button>:""
+      }
             
       <PopupState variant="popover" popupId="demo-popup-menu">
         {popupState => (
           <React.Fragment>
+            
             <IconButton aria-label="account of current user" aria-controls="simple-menu" aria-haspopup="true" {...bindTrigger(popupState)}
               color="inherit"  
             >
               
-            <Typography variant="h6" component="h6"style={{ position: 'relative' }}>{this.state.firstName} {this.state.lastName}</Typography>
-              <AccountCircle/>
+              <Typography variant="h6" component="h6"style={{ position: 'relative' }}>{this.state.firstName} {this.state.lastName}</Typography>
+                <AccountCircle/>
             </IconButton>
+
+            {/* I left this <Menu> tag below just to fill space...using the dropdown, because I think we could use it in the future. */}
+            {/* But it is currently useless. */}
             <Menu {...bindMenu(popupState)}>
                 <MenuItem onClick={this.toProfile}>Profile</MenuItem>
-                <MenuItem onClick={this.toSearch}>Search Users</MenuItem>
-                <MenuItem onClick="null">Messages</MenuItem>
                 {(this.state.isMentor!=null)&& (localStorage.usertoken!=null)?
                 <MenuItem onClick={this.toContentCreation.bind(this)}>Content Creation</MenuItem>:""}
                 <MenuItem onClick={this.handleLogout.bind(this)}>Logout</MenuItem>
             </Menu>
+
           </React.Fragment>
         )}
       </PopupState>
-  
+      
+      <Button color="inherit" onClick = {this.handleLogout.bind(this)}> Logout </Button>
             
-          </div>
+    </div>
   )
     return(
         <div className ={classes.root}>   
             <AppBar position="static">
                 <Toolbar>
-                <img src={Logo} alt="websitelogo"/>
-                <Typography variant="h4" className={classes.title} style={{  width: '80%', position: 'relative' }}>
-                    GIDDY-UP
-                    <Button color="inherit" onClick = "null">Home </Button>
-                    {localStorage.usertoken!=null ? <Button color="inherit" onClick="null">Newsfeed</Button>:""}
-                    <Button color="inherit" onClick = "null"> Contact Us </Button> 
-                </Typography>
-                {localStorage.usertoken ? LogoutProfile : LoginRegSwitch }
+                  <img src={Logo} alt="websitelogo"/> 
+                  <Typography className={classes.title} style={{  width: '40%', position: 'relative' }}> 
+                    <Button color="inherit" onClick={this.toProfile}> <h3>GIDDY-UP</h3> </Button>
+                  </Typography>
+                  {localStorage.usertoken!=null ? <Button color="inherit" onClick="null">Newsfeed</Button>:""}
+                  <Button color="inherit" onClick = "null"> Contact Us </Button>
+                  {localStorage.usertoken ? LogoutProfile : LoginRegSwitch }
+
+                  {/* <Typography> 
+                    {localStorage.usertoken ? LogoutProfile : LoginRegSwitch }
+                  </Typography> */}
                 </Toolbar>
             </AppBar>
         </div>
     )
     }
 }
-
+// style={{  width: '50%', position: 'relative' }}
 export default withRouter(Navbar);
