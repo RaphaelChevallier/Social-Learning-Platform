978c79e08545ff8778ac946fe5695438e36cead5
tyreln <tyrelnarciso@hotmail.com>
2020-04-15 17:37:42 -0700

    improved landing page (#22)
    * improved landing page
    
    * revert package versions
    
    Co-authored-by: Raphael Chevallier <raphaelchevallier@hotmail.com>

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { styled } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-
+import logo from "../images/logo.jpg";
+import picture1 from "../images/Findyourtribe2.jpg";
+import picture2 from "../images/Findyourtribe.jpg";
+import picture3 from "../images/GiddyUpPitch001b.gif";
 
 const MyButton = styled(Button)({
 //   background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
@@ -38,6 +41,8 @@ const titleStyle = ({
     textAlign: "center",
  
     fontSize :"2rem",
+    marginBottom: "2rem",
+   
    
   });
   const paragraph= ({
@@ -51,6 +56,36 @@ const titleStyle = ({
     
    
   });
+  const formatted= ({
+    
+    display: "flex",
+    marginLeft: "auto",
+    marginRight: "auto",
+    float: "clear",
+ height: "90vh",
+ width: "100vw",
+alignItems: "center",
+justifyContent: "center",
+    
+   
+    
+   
+  });
+
+  const formatted2= ({
+    display: "flex",
+    marginLeft: "auto",
+    marginRight: "auto",
+//  height: "60vh",
+//  width: "100vw",
+alignItems: "center",
+justifyContent: "center",
+    
+   
+    
+   
+  });
+
 
 
 
@@ -59,10 +94,27 @@ export default function StyledComponents() {
   return(
   //<MyButton>Styled Components</MyButton>
   <div>
-      <h1 style = {titleStyle}>Giddy-Up</h1>
-      <h2 style = {subtitle}>Fast-Track your way into a new career!</h2>
+    <div style = {formatted}> 
+    <img src={logo} alt="logo" style={{ width: '40%', height: '20%' }} />
+    
+    </div>
+       
+    <h2 style = {subtitle}>Fast-Track your way into a new career!</h2>
+      <div style = {formatted2}> 
+    <img src={picture3 } alt="pitchpic" style={{ width: '40%', height: '30%' }} />
+    </div>
       <section>
-      <p style = {paragraph}>Giddy-Up is a startup project dedicated to creating tools and connections that will provide all the resources you need to get up and running in a new career, whether you have experience with your goals or not.</p>
+      <p style = {paragraph}>Giddy-Up is a startup project dedicated to creating tools and connections that will provide all the resources you need to get up and running in a new career. No matter your experiene level, our goal is to provide a platform for people of any skill level to improve and thrive as quick as possible.</p>
+      <div style = {formatted2}> 
+    <img src={picture1} alt="pitchpic" style={{ width: '40%', height: '30%' }} />
+    </div>
+      </section>
+     
+      <section> 
+      <p style = {paragraph}>Even with the countless ways to connect with others available right now, we found there was still a lack of platforms that exist solely to ease and enhance the path to a newfound talent or skill that you can use to kickstart a new career. Giddy-Up is being created to facilitate and support those across the world that wish to learn whatever they wish to with other like-minded and capable individuals.</p>
+      <div style = {formatted2}> 
+    <img src={picture2} alt="pitchpic" style={{ width: '40%', height: '30%' }} />
+    </div>
       </section>
 
 
